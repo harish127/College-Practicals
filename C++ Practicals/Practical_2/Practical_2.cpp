1b93df5cee30c33e5f97ed62413f01873ce00553
harish <harishkaiwart127@gmail.com>
2021-09-20 15:18:54 +0530

    implemented test_num() case a done. Yay!!

--- C++ Practicals/Practical_2/Practical_2.cpp
+++ C++ Practicals/Practical_2/Practical_2.cpp
@@ -12,7 +12,13 @@ Date: 20/9/2021
 using namespace std;
 
 int test_num(int n1,int n2){
-    return (n1>n2);
+    switch (n1>n2)
+    {
+    case 0:
+        return n2;
+    case 1:
+        return n1;
+    } ;
 }
 
 int main(){
@@ -23,6 +29,7 @@ int main(){
     //Code for Main Menu
     while(ch!='d'){
         cout<<"(a) Two numbers \n(b) Three Numbers \n(c) Four numbers \n(d) Exit";
+        cout<<"\nEnter a valid Choice: ";
         cin>>ch;
 
         switch (ch)
@@ -30,9 +37,8 @@ int main(){
         case 'a':
             cout<<"\nEnter two numbers: ";
             cin>>a>>b;
-
             res = test_num(a,b);
-            cout<<"\n Gratest Number is "<<res;
+            cout<<"\n Gratest Number is "<<res<<"\n";
             break;
         
         default:
