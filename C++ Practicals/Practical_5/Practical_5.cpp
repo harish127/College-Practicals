e0118b3db5d86456eec624fd293a0024d8cea899
harish <harishkaiwart127@gmail.com>
2021-10-03 19:23:29 +0530

    template ready.

---
+++ C++ Practicals/Practical_5/Practical_5.cpp
@@ -0,0 +1,14 @@
+/*
+
+Write a Program to read the element of the given two matrixes & to perform the matrix multiplication.
+
+Coded by: Harish Kumar
+Date: 3/10/2021
+
+*/
+
+#include<iostream>
+
+int main(){
+    
+}
