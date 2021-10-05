82cc7d49b1242c7e97f5670ac1de4604b4464e1d
harish <harishkaiwart127@gmail.com>
2021-10-05 16:36:02 +0530

    rows and clumn issue fixed

--- C++ Practicals/Practical_5/Practical_5.cpp
+++ C++ Practicals/Practical_5/Practical_5.cpp
@@ -7,26 +7,43 @@ Date: 3/10/2021
 
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void read_matrix(int **arr, int r,int c){
-    for(int i=0;i<r;i++){
-        for(int j=0;j<c;j++){
-            cin>>arr[i][j];
+// void read_matrix(int arr, int r,int c){
+//     for(int i=0;i<r;i++){
+//         for(int j=0;j<c;j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+// }
+
+int main()
+{
+    int r1, r2, c1, c2;
+    cout << "Enter number row & column of first matrix: ";
+    cin >> r1 >> c1;
+    int arr1[r1][c1];
+    cout << "Enter Elements of first matrix: ";
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            cin >> arr1[i][j];
         }
     }
-}
 
-int main(){
-    int r1,r2, c1,c2;
-    cout<<"Enter number row & column of first matrix: ";
-    cin>>r1>>c1>>r2>>c2;
-    int **arr1 = new int [r1][c1];
-    cout<<"Enter Elements of first matrix: ";
-    read_matrix(arr1,r1,c1);
-        int **arr2 = new int [r2][c2];
-    cout<<"Enter Elements of first matrix: ";
-    read_matrix(arr1,r2,c2);
+    cout << "Enter number row & column of second matrix: ";
+    cin >> r2 >> c2;
+
+    int arr2[r2][c2];
+    cout << "Enter Elements of second matrix: ";
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            cin >> arr2[i][j];
+        }
+    }
 
 }
